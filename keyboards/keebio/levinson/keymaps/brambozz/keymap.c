ac26128501ee5ca3fdfda452864818fe27baa6f2
brambozz <brambo5@live.nl>
2020-07-13 14:48:04 +0200

    Switch to RAISE with space when held

--- keyboards/keebio/levinson/keymaps/brambozz/keymap.c
+++ keyboards/keebio/levinson/keymaps/brambozz/keymap.c
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------'
  */
 [COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,      KC_G,    KC_J,    KC_L,      KC_U,    KC_Y,    KC_SCLN, RESET, \
-  RGUI_T(KC_BSPC), KC_A,    KC_R,    KC_S,    KC_T,      KC_D,    KC_H,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  LSFT_T(KC_ESC),  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,    KC_K,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-  MO(ADJUST),      KC_LCTL, KC_LALT, KC_LGUI, MO(LOWER), KC_SPC,  KC_SPC,  MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,          KC_Q,    KC_W,    KC_F,    KC_P,      KC_G,             KC_J,    KC_L,      KC_U,    KC_Y,    KC_SCLN, RESET, \
+  RGUI_T(KC_BSPC), KC_A,    KC_R,    KC_S,    KC_T,      KC_D,             KC_H,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  LSFT_T(KC_ESC),  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,             KC_K,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
+  MO(ADJUST),      KC_LCTL, KC_LALT, KC_LGUI, MO(LOWER), LT(RAISE,KC_SPC), KC_SPC,  MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower/raise
